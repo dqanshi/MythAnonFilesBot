9ca96101fd7f5b5a527b9e71fef22cf7a56b4050
DragonPower84 <82083924+DragonPower84@users.noreply.github.com>
2021-06-25 09:34:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,14 @@ import os
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-    APP_ID = int(os.environ.get("APP_ID", 1234))
+    APP_ID = int(os.environ.get("APP_ID", ""))
 
     API_HASH = os.environ.get("API_HASH", "")
 
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "Kai84_Space")
+
+
+    UPDATES_CHANNEL_2 = os.environ.get("UPDATES_CHANNEL_2", "Anime_Troop")
+    
+
+    UPDATES_CHANNEL_3 = os.environ.get("UPDATES_CHANNEL_3", "Movie_Bank")
