45e069657a53c7f2f3e591a24b76323c205c0208
DragonPower84 <82083924+DragonPower84@users.noreply.github.com>
2021-07-04 14:17:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,3 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
 
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "Kai84_Space")
-
-
-    UPDATES_CHANNEL_2 = os.environ.get("UPDATES_CHANNEL_2", "Anime_Troop")
-    
-
-    UPDATES_CHANNEL_3 = os.environ.get("UPDATES_CHANNEL_3", "Movie_Bank")
